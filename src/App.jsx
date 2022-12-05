72f2b9ca8920f5bef56a0b183dd635773751ae3e
Dawid Skowronski <dskwr@protonmail.com>
2022-12-05 20:58:14 +0100

    Calling to opentdb for questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
 import React from "react"
-import {useState} from "react"
+import { useState, useEffect } from "react"
 import Overlay from "./components/Overlay"
 
 
 export default function App() {
     const [game, setGame] = useState(false)
+    const [questions, setQuestions] = useState([])
+    useEffect(() => {
+        fetch('https://opentdb.com/api.php?amount=5&category=9&type=multiple')
+        .then(response => response.json())
+        .then(data => {
+            if(data.response_code === 0){
+                let all_questions = data.results
+                setQuestions(
+                    all_questions.map(question => {
+                        const correct_answer = {
+                            answer: question.correct_answer,
+                            correct: true,
+                            selected: false
+                        }
+                        const answers = question.incorrect_answers.map(answer => (
+                            {
+                                answer: answer,
+                                correct: false,
+                                selected: false
+                            }
+                        ))
+                        answers.splice(Math.floor(Math.random() * (answers.length + 1)), 0, correct_answer)
 
+                        return {
+                            question: question.question,
+                            answers: answers
+                        }
+                    })
+                )
+            }
+        })
+    }, [game])
+    
     return (
         <div>
             {game ? 
